5c88dffbae2f0d09091b4f2545a60adc542a75d1
Marina Chevis <marinachevis@gmail.com>
2022-03-01 19:46:30 -0500

    product form fields

--- client/components/ProductForm.js
+++ client/components/ProductForm.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const ProductForm = () => {
   const { id } = useParams();
@@ -18,26 +19,66 @@ const ProductForm = () => {
     setProductAttributes(productAttributes);
   }, []);
 
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  console.log(watch("Name-priority")); // watch input value by passing the name of it
+
   return (
-    <div>
-      <div>Source: {product.brand}</div>
-      <div>URL: {product.url}</div>
-      <div>
-        {productAttributes.map((pa) => (
-          <div>
-            <h4>{pa.attribute.name}</h4>
-            <ul>
-              <li>{pa.value}</li>
-              <li>
-                {pa.attribute.priority || pa.attribute.priority === 0
-                  ? `P${pa.attribute.priority}`
-                  : ""}
-              </li>
-            </ul>
+    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+    <form onSubmit={handleSubmit(onSubmit)} className="product--form">
+      <label>Source:</label>
+      <input
+        defaultValue={product.brand}
+        {...register("source", { required: true })}
+      />
+      {errors.exampleRequired && <span>This field is required</span>}
+      <label>URL:</label>
+      <input
+        defaultValue={product.url}
+        {...register("url", { required: true })}
+      />
+      {errors.exampleRequired && <span>This field is required</span>}
+
+      {productAttributes.map((pa) => (
+        <div key={pa.id}>
+          <h4>{pa.attribute.name}</h4>
+          <div className="product--form--attribute">
+            <label>Value:</label>
+            <input
+              defaultValue={pa.value}
+              {...register(`${pa.attribute.name}-value`, { required: true })}
+            />
+            <label>Priority:</label>
+            <select
+              name="Priority"
+              defaultValue={
+                pa.attribute.priority || pa.attribute.priority === 0
+                  ? pa.attribute.priority
+                  : -1
+              }
+              {...register(`${pa.attribute.name}-priority`, { required: true })}
+            >
+              <option value={-1} hidden disabled>
+                Select a priority
+              </option>
+              <option value={0}>P0 - Most important</option>
+              <option value={1}>P1</option>
+              <option value={2}>P2</option>
+              <option value={3}>P3</option>
+              <option value={4}>P4 - Least important</option>
+              <option value={-99}>I don't want to prioritize this</option>
+            </select>
           </div>
-        ))}
-      </div>
-    </div>
+        </div>
+      ))}
+      <input type="submit" />
+    </form>
   );
 };
 
